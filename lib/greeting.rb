3086d19927368e5e551627aff59f98330550eebd
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-10 12:15:14 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,9 +2,7 @@
 name = "Sally"
 def greeting(name)
   puts "Hi! I'm #{name}, what's your name?\n"
-end
-greeting(name)
-def meet(name)
   puts "Hello #{name}. It's nice to meet you."
 end
-meet(name)
+greeting(name)
+
